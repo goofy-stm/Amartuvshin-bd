be3f4723f94ad8503f2b5928ad196c52cf9156cd
goofy-stm <goofy.stm@gmail.com>
2026-02-27 19:38:42 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("aavaa!", 28, 10);
+            ctx.fillText("Birthday       !", 28, 10);
             ctx.restore();
         },
         clear: function() {
@@ -1080,3 +1080,4 @@ function timeElapse(date) {
 }
 
 })(window);
+
